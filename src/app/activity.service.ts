383f55b0687547f5defc1aa8fcc6c24f7f8badd0
AbdulmajeedRu <44317320+AbdulmajeedRu@users.noreply.github.com>
2021-08-11 10:26:09 +0300

    Activity services

---
+++ src/app/activity.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getActivity(activityID: string){
+    return this._httpClient.get(API + "/id/" + activityID);
+
+  }
+
+  getAllActivities(){
+    return this._httpClient.get(API);
+  }
+}
+
+const API = "https://orangevallyecaa.org/api/videos"
